9d31d2138693b134394afea7c8598750ce2b58f2
Guilherme Teixeira <ggiacomini2012@gmail.com>
2025-04-10 08:33:40 -0300

    refactor: remover arquivo macro-cell-class.js

--- macro-cell-class.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const allTds = document.querySelectorAll('td');
-  allTds.forEach(td => {
-    if (td.querySelector('table')) {
-      td.classList.add('each-macro-cell');
-    }
-  });
-}); 
